a8df6a999eb8c19953b4a83ea65ecc0ca923055b
Pugma <maronmarondelicious@gmail.com>
2024-05-24 19:02:10 +0900

    made schema

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,9 +1,33 @@
-CREATE TABLE IF NOT EXISTS `schedule` (
-    `groupId` VARCHAR(32) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users`(
+    `userId` INT(11) NOT NULL AUTO_INCREMENT,
+    `userName` VARCHAR(50) NOT NULL UNIQUE,
+    `password` BINARY(32) NOT NULL,
+    PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `groups` (
+    `groupId` INT(11) NOT NULL AUTO_INCREMENT,
+    `groupUuid` VARCHAR(32) NOT NULL,
+    `groupName` VARCHAR(32) NOT NULL,
     `userName` VARCHAR(50) NOT NULL,
-    `year` TINYINT(4) NOT NULL,
-    `month` TINYINT(2) NOT NULL,
-    `week` TINYINT(1) NOT NULL,
+    `lastUpdate` DATETIME NOT NULL,
+    PRIMARY KEY (`groupId`),
+    INDEX (`lastUpdate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `userGroup` (
+    `userId` INT(11) NOT NULL,
+    `groupId` INT(11) NOT NULL,
+    INDEX (`userId`, `groupId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `schedule` (
+    `groupId` INT(11) NOT NULL,
+    `userName` VARCHAR(50) NOT NULL,
+    `since` DATETIME NOT NULL,
+    `until` DATETIME NOT NULL,
     `condition` TINYINT(1) NOT NULL,
-    PRIMARY KEY (`groupId`)
+    `lastUpdate` DATETIME NOT NULL,
+    PRIMARY KEY (`groupId`),
+    INDEX (`lastUpdate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
