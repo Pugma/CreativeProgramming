7d404e1d3f66d39b399c3324b29c9977a73f78d7
Pugma <132571355+Pugma@users.noreply.github.com>
2024-07-27 11:21:59 +0900

    fixed importing api files

--- client/src/components/SignUp.vue
+++ client/src/components/SignUp.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import apis from '@/lib/apis'
-import { type PostLogin } from '@/lib/apis'
+import apis from '@/lib/apis/index'
+import { type PostLogin } from '@/lib/apis/index'
 import PageHeader from '@/components/PageHeader.vue'
 import PageContainer from '@/components/PageContainer.vue'
 
