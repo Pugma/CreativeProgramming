de73e65f002d5702d25c007ae43d3327df793713
Pugma <maronmarondelicious@gmail.com>
2024-07-27 16:30:56 +0900

    created MyPage and GroupPage / activate Suspense and prism

---
+++ client/src/views/MyPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PageHeader from '@/components/PageHeader.vue'
+import PageContainer from '@/components/PageContainer.vue'
+import apis, { type GroupItem } from '@/lib/apis'
+import { ref } from 'vue'
+import GroupList from '@/components/GroupList.vue'
+import PageTitle from '@/components/PageTitle.vue'
+
+const groups = ref<GroupItem[]>([])
+groups.value = (await apis.user.meGet()).data
+</script>
+
+<template>
+  <PageHeader />
+  <PageContainer>
+    <PageTitle title="所属しているグループ一覧" />
+    <GroupList :groups="groups" />
+  </PageContainer>
+</template>
+
+<style lang="scss" scoped></style>
