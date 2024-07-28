3f756f8a49eb04fb92ff440704a55fbd25fe2e7f
Pugma <maronmarondelicious@gmail.com>
2024-07-29 00:57:22 +0900

    fixed design / added me query

--- client/src/components/ScheduleInput.vue
+++ client/src/components/ScheduleInput.vue
@@ -12,27 +12,39 @@ const newSchedule = ref<ScheduleItem>({
   Condition: 0
 })
 
-apis.schedule.scheduleGroupIdPost(groupId.value, newSchedule.value)
+const sendReq = () => {
+  apis.schedule.scheduleGroupIdPost(groupId.value, newSchedule.value)
+}
 </script>
 
 <template>
   <section>
     <PageTitle title="自分の忙しさを登録する" />
     <input type="date" :class="$style.date" /> ~ <input type="date" :class="$style.date" />
-    <div>
-      <input type="radio" id="0" :value="0" v-model="newSchedule.Condition" />
-      <label for="0">めちゃ余裕</label>
-      <input type="radio" id="1" :value="1" v-model="newSchedule.Condition" />
-      <label for="1">余裕がなくはない</label>
-      <input type="radio" id="2" :value="2" v-model="newSchedule.Condition" />
-      <label for="2">どちらでもない</label>
-      <input type="radio" id="3" :value="3" v-model="newSchedule.Condition" />
-      <label for="3">そこまでゆとりがない</label
-      ><input type="radio" id="3" :value="3" v-model="newSchedule.Condition" />
-      <label for="3">マジでゆとりがない</label>
+    <div :class="$style.input">
+      <div>
+        <input type="radio" id="0" :value="0" v-model="newSchedule.Condition" />
+        <label for="0">めちゃ余裕</label>
+      </div>
+      <div>
+        <input type="radio" id="1" :value="1" v-model="newSchedule.Condition" />
+        <label for="1">余裕がなくはない</label>
+      </div>
+      <div>
+        <input type="radio" id="2" :value="2" v-model="newSchedule.Condition" />
+        <label for="2">どちらでもない</label>
+      </div>
+      <div>
+        <input type="radio" id="3" :value="3" v-model="newSchedule.Condition" />
+        <label for="3">そこまでゆとりがない</label>
+      </div>
+      <div>
+        <input type="radio" id="3" :value="3" v-model="newSchedule.Condition" />
+        <label for="3">マジでゆとりがない</label>
+      </div>
     </div>
     <div>
-      {{ newSchedule.Condition }}
+      <button @click="sendReq">送信</button>
     </div>
   </section>
 </template>
@@ -41,8 +53,24 @@ apis.schedule.scheduleGroupIdPost(groupId.value, newSchedule.value)
 section {
   padding: 8px;
 }
+
 .date {
   font-size: 1.2rem;
   width: 16rem;
+}
+
+.input {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 8px;
+  div {
+    margin: {
+      left: 4px;
+      right: 4px;
+    }
+  }
+  label {
+    font-size: 1.2rem;
+  }
 }
 </style>
